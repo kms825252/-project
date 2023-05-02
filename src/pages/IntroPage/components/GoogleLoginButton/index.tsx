8b769472e1a22cb7d2a7ea235a0f2ba2d9c07c40
minsu_kim <minsu8252@naver.com>
2023-05-02 18:01:54 +0900

    리덕스 사용

--- src/pages/IntroPage/components/GoogleLoginButton/index.tsx
+++ src/pages/IntroPage/components/GoogleLoginButton/index.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import CLIENT_KEY from "constants/keys";
-import { default as axios } from "axios";
-import { useNavigate } from "react-router-dom";
-import HomePage from "../../../HomePage";
 
 const GoogleLoginButton = () => {
-  const navigate = useNavigate();
   const clientId = CLIENT_KEY.GOOGLE;
   return (
     <>
@@ -15,8 +11,6 @@ const GoogleLoginButton = () => {
         <GoogleLogin
           onSuccess={(res) => {
             console.log(res);
-            // navigate("/home");
-            // axios.get("http://localhost:8080/oauth2/authorization/google");
           }}
           onError={() => {
             console.log("로그인 실  패");
